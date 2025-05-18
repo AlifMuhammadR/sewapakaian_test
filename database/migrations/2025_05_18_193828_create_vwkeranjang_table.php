474026350f6ae098e805a883c8b1e1c44eef1910
alif.mrizky2007@gmail.com <alif.mrizky2007@gmail.com>
2025-05-19 02:47:53 +0700

    add vw keranjang

---
+++ database/migrations/2025_05_18_193828_create_vwkeranjang_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("
+        CREATE VIEW vwkeranjang AS
+        SELECT
+            k.id,
+            k.id_pelanggan,
+            k.id_pakaian,
+            p.foto1,
+            p.nama AS nama_pakaian,
+            p.harga_sewa,
+            k.jumlah_order,
+            (p.harga_sewa * k.jumlah_order) AS subtotal,
+            k.created_at,
+            k.updated_at
+        FROM
+            keranjang k
+        JOIN
+            pakaian p ON k.id_pakaian = p.id
+    ");
+    }
+
+    public function down(): void
+    {
+        DB::statement("DROP VIEW IF EXISTS vwkeranjang");
+    }
+};
